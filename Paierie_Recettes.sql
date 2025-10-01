ba54b83d2c4e108c902855b2c5c722062f368cff
Mohamed S. Cedric Traore <mohamed.s.c.traore@gmail.com>
2025-10-01 14:56:02 +0000

    Test modification

--- Paierie_Recettes.sql
+++ Paierie_Recettes.sql
@@ -5,7 +5,7 @@ and a.spec1 = b.ide_cpt
 and a.Ide_Gest = '2025'
 and a.Ide_Poste in (select ide_poste from rm_poste where ide_typ_poste = 'P')
 And Flg_Cptab = 'O'
-And Dat_Ecr <= '30/09/2025'
+And Dat_Ecr <= '31/10/2025'
 And Spec1 Like '7%'
 group by a.Ide_Poste, c.libn, a.Spec1, b.libn
 Order By 3,1
